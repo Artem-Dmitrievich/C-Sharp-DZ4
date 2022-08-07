a92a949f47f956b2ee8dcb8edd41be0bf3857f0b
kozlov artem <artemko1987@yandex.ru>
2022-08-07 10:20:27 +0300

    Задача27

---
+++ Задача27/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+Console.Write("Входящее число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int summa = 0;
+
+while (a > 0)
+{
+  int num = a % 10;
+  a = a / 10;
+  summa = summa + num;
+}
+Console.WriteLine("Сумма: " + summa);
